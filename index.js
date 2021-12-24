c3e2002b516c32cd50506d5a0d0593f659167f2c
Justin Jeffries <justincyjeffries@gmail.com>
2021-12-24 02:32:29 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const messages = [];
+function writeCards(names, event){
+    for (let i = 0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;  }
+    return messages;
+}
+
+function countDown(){
+    let i = 10;
+    while (i >=0) {
+        console.log(i--);
+    }
+    return i;
+}
